348f09559cfc06476f807cb271820078ec002d1a
cisconomadic <cisconomadic@gmail.com>
2025-10-22 01:04:57 -0500

    Update Terraform configuration for Azure provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-}
-
-variable "location" {
-  description = "Azure region"
-  default     = "East US"
-}
-
-variable "resource_group_name" {
-  description = "Resource group name"
-  default     = "ipsec-vpn-rg"
 }
